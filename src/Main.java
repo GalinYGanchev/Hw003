40b5645d00527a6cf205344c6f7cc5229c6c2dcb
group <galin_psy@abv.bg>
2016-02-23 01:55:33 +0200

    Home work

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.println("Insert integer1:");
+		int number1 = in.nextInt();
+		System.out.println("");
+		System.out.println("Insert integer2:");
+		int number2 = in.nextInt();
+		System.out.println("");
+	}
+
+}
